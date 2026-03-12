470d2aa435f4e576bfafe6f92816139879dad613
Ch3333zzz <159568350+Ch3333zzz@users.noreply.github.com>
2026-03-12 19:24:38 +0300

    First task is done

--- lab1/app/src/main/java/org/example/Calculator.java
+++ lab1/app/src/main/java/org/example/Calculator.java
@@ -2,8 +2,31 @@ package org.example;
 
 public class Calculator {
 
-    public double asin(double x) {
+    private final double ERROR = 1e-10;
+    private final int MAX_ITER = 100000;
 
-        return 0; //just for creating tests
+    public Double asin(double x) {
+        if (x > 1 || x < -1)
+            return Double.NaN;
+
+        if (x == 1)
+            return Math.PI/2;
+        if (x == -1)
+            return -Math.PI/2;
+
+        double sum = x;
+        double term = x; // current term
+
+        for (int i = 1; i < MAX_ITER; i++) {
+            double multiplier = (x * x * (2 * i - 1) * (2 * i - 1)) / 
+                                (2.0 * i * (2 * i + 1));
+            
+            term *= multiplier;
+            sum += term;
+
+            if (Math.abs(term) < ERROR)
+                break;
+        }
+        return sum;
     }
 }
